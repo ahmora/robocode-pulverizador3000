1f9f443ab18884cf35c0d839c3591a17d2f013b1
ahmora <alejandrohmora@ciencias.unam.mx>
2014-10-09 00:37:47 -0500

    nuevo

---
+++ src/pulverizador/Practica03.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pulverizador;
+
+import robocode.control.*;
+import robocode.control.events.*;
+
+/**
+ *
+ * @author Alejandro Hernandez Mora
+ */
+public class Practica03 {
+
+    public static void main(String[] args) {
+
+        Pulverizador3000 nuevo = new Pulverizador3000();
+        // Disable log messages from Robocode
+        RobocodeEngine.setLogMessagesEnabled(false);
+
+         // Create the RobocodeEngine
+        //   RobocodeEngine engine = new RobocodeEngine(); // Run from current working directory
+        RobocodeEngine engine = new RobocodeEngine(new java.io.File("/home/alex/robocode")); // Run from C:/Robocode
+
+        // Add our own battle listener to the RobocodeEngine 
+        engine.addBattleListener(new BattleObserver());
+
+        // Show the Robocode battle view
+        engine.setVisible(true);
+        
+
+         // Setup the battle specification
+        int numberOfRounds = 5;
+        BattlefieldSpecification battlefield = new BattlefieldSpecification(800, 600); // 800x600
+        RobotSpecification[] selectedRobots = engine.getLocalRepository("sample.RamFire,sample.Corners,sample,pulverizador.Pulverizador3000*");
+
+        BattleSpecification battleSpec = new BattleSpecification(numberOfRounds, battlefield, selectedRobots);
+
+        // Run our specified battle and let it run till it is over
+        engine.runBattle(battleSpec, true); // waits till the battle finishes
+
+        // Cleanup our RobocodeEngine
+        engine.close();
+
+        // Make sure that the Java VM is shut down properly
+        System.exit(0);
+    }
+}
+
+ //
+// Our private battle listener for handling the battle event we are interested in.
+//
+class BattleObserver extends BattleAdaptor {
+
+    // Called when the battle is completed successfully with battle results
+    public void onBattleCompleted(BattleCompletedEvent e) {
+        System.out.println("-- Battle has completed --");
+
+        // Print out the sorted results with the robot names
+        System.out.println("Battle results:");
+        for (robocode.BattleResults result : e.getSortedResults()) {
+            System.out.println("  " + result.getTeamLeaderName() + ": " + result.getScore());
+        }
+    }
+
+    // Called when the game sends out an information message during the battle
+    public void onBattleMessage(BattleMessageEvent e) {
+        System.out.println("Msg> " + e.getMessage());
+    }
+
+    // Called when the game sends out an error message during the battle
+    public void onBattleError(BattleErrorEvent e) {
+        System.out.println("Err> " + e.getError());
+    }
+}
---
+++ src/pulverizador/Pulverizador3000.java
@@ -0,0 +1,122 @@
+
+package pulverizador;
+
+
+/**
+ *
+ * @author Alejandro Hernandez Mora
+ */
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * Alex - a robot by (your name here)
+ */
+public class Pulverizador3000 extends Robot
+{
+    
+        String run, onScannedRobot, onHitByBullet,onHitWall;
+ 	String individuo;
+	
+        
+	
+	public Pulverizador3000(){
+		super();
+                
+                individuo=readIndividuo();
+                llenaAcciones();
+	}
+        
+        public String readIndividuo(){
+            Scanner in;
+            String line="";
+            try {
+                in = new Scanner(new FileReader("individuo.txt"));
+                line= in.nextLine();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Pulverizador3000.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println(line);
+            
+            return line;
+        }
+        
+        public void llenaAcciones(){
+            String [] acciones= individuo.split(",");
+            run=acciones[0];
+            onScannedRobot=acciones[1];
+            onHitByBullet=acciones[2];
+            onHitWall=acciones[3];
+            
+        }
+        
+        public void realizaAccion(int accion){
+            switch(accion){
+                case 0: ahead(100); break;
+                case 1: back(100); break;
+                case 2: fire(10); break;
+                case 3: fire(2); break;
+                case 4: turnGunRight(70); break;
+                case 5: turnGunLeft(70); break;
+                
+                default:    fire(2);
+            }
+        }
+
+	/**
+	 * run: Alex's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			int i=0;
+			while(i < run.length()){
+			// Replace the next 4 lines with any behavior you would like
+				realizaAccion(i);
+				ahead(100);
+				turnGunRight(360);
+				back(100);
+				turnGunRight(360);
+				i++;
+			}
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
